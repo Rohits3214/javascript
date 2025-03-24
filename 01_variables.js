93a69f871550fda8709a05f70a9cc05537ceab53
Rohits3214 <rohitcse3214@gmail.com>
2025-03-25 00:10:37 +0530

    datatypes

---
+++ 01_variables.js
@@ -0,0 +1,7 @@
+const accountId= 12346;// cant be changed
+let accountName ="rohitsingh"//can be changed
+var accountStatus= "active";//dont use var
+accountState="jabalpur"// global variable
+
+console.table([accountId,accountName,accountStatus,accountState])
+
---
+++ 02_datatypes.js
@@ -0,0 +1,11 @@
+let name ="rohit"//string value prefer double quotes
+let age=41//integer value less than 2^53
+let ismarried=true//boolean true/false
+let city=null//standalone value
+let state;//undefined
+
+console.log(typeof city)
+console.log(typeof state)
+console.log(typeof "rohit")
+
+
