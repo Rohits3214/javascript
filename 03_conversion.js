4336f94ab1aa29bf037b3f91e04e51657f891ff0
Rohits3214 <rohitcse3214@gmail.com>
2025-03-25 00:51:08 +0530

    conversions

---
+++ 03_conversion.js
@@ -0,0 +1,26 @@
+let Score= true
+console.log(Score)
+console.log(typeof Score)
+
+let valueInNumber = Number(Score)
+console.log(valueInNumber)
+console.log(typeof valueInNumber)
+
+// 33abc= Nan(not a number is output but datatype is nuber)
+ // null= 0 is our output
+ //true/false=1/0 is our output
+
+ let isLoggedIn = 1
+ let booleanIsLoggedIn= Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn)
+
+ //o =false
+ //""= false
+ //null=False
+ //"rohit"=true
+ //2=true
+
+ let number=11
+ let stringNumber=String(number)
+ console.log(stringNumber)
+ console.log(typeof stringNumber)
